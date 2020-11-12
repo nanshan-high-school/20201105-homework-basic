d3304a2330e95fa4631edd7ffeef44a90216e82b
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2020-11-12 18:45:16 +0800

    Update grouping-array.cpp

--- grouping-array.cpp
+++ grouping-array.cpp
@@ -5,17 +5,28 @@ int main () {
     int c, p[c], n;
     cin >> c >> n;
     for(int i = 0; i <= c; i++) {
-        cin >> p[c];
+        cin >> p[c - 1];
     }
 
     int maxteamvalue = -1, nowteamvalue, output;
 
     for(int i = 3; i <= (c/3); i = i+3) {
+        if(c / 3 == 0) {
         nowteamvalue = p[i - 3] + p[i - 2] + p[i - 1];
         if(nowteamvalue >= maxteamvalue) {
             maxteamvalue = nowteamvalue;
             output = i/3;
         }
+        }else if ((i - 2) / 3 == 0) {
+            nowteamcvalue = p[i - 3]
+            if(nowteamvalue >= maxteamvalue) {
+            maxteamvalue = nowteamvalue;
+            output = i/3;
+        }else {
+            nowteamcvalue = p[i - 2]
+            if(nowteamvalue >= maxteamvalue) {
+            maxteamvalue = nowteamvalue;
+            output = i/3;
     }
     cout << maxteamvalue << output;
 }
